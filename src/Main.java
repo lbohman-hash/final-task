7eac48f4f7ff45845c96a5e86fbac0d289a99c24
Heierson <robert.heierson@gmail.com>
2021-06-21 14:44:23 +0200

    divMethodDone

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,13 @@ public class Main {
 
         System.out.println("Please enter your first number: ");
         int num2 = scanner.nextInt();
+
+        System.out.println(divMethod(num1, num2));
+
+    }
+
+    static int divMethod(int num1, int num2) {
+        int divResult = num1/num2;
+        return divResult;
     }
 }
