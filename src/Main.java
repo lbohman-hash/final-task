b774295a3805c7f0c4042e09e4ad643e3f255757
Johanna Högström <johanna.hogstrom@hotmail.com>
2021-06-21 14:28:29 +0200

    Multiplications method added

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,11 @@ public class Main {
         System.out.println("Please enter your first number: ");
         int num2 = scanner.nextInt();
 
-        subMethod(num1, num2);
+
         multiplication(num1, num2);
     }
 
-     static int subMethod(int num1, int num2) {
-         int differens = num1 - num2;
-         return differens;
-    }
+
 
     static int multiplication(int num1, int num2) {
         int product = num1 * num2;
