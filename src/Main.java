f9785747c57b0386bdff0616419d7b175f0b6148
Heierson <robert.heierson@gmail.com>
2021-06-21 14:06:17 +0200

    subMethod added

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,12 @@ public class Main {
 
         System.out.println("Please enter your first number: ");
         int num2 = scanner.nextInt();
+
+        subMethod(num1, num2);
+    }
+
+     static int subMethod(int num1, int num2) {
+         int differens = num1 - num2;
+         return differens;
     }
 }
