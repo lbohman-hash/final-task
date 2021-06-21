5bbe15e862a6d72f4eeb83698393753a26c64813
Johanna Högström <johanna.hogstrom@hotmail.com>
2021-06-21 14:22:32 +0200

    Multiplication method added

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,16 @@ public class Main {
         int num2 = scanner.nextInt();
 
         subMethod(num1, num2);
+        multiplication(num1, num2);
     }
 
      static int subMethod(int num1, int num2) {
          int differens = num1 - num2;
          return differens;
+    }
+
+    static int multiplication(int num1, int num2) {
+        int product = num1 * num2;
+        return product;
     }
 }
